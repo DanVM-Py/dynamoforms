ebb8f1049492f208b15d1456aef45cd94597d426
Daniel Velez <daniel.velez@rappi.com>
2025-04-08 13:53:45 +0000

    Cli System with Supabase Types

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -234,10 +234,8 @@ export type Database = {
           access_level: string | null
           activated_at: string | null
           created_at: string | null
-          created_by: string | null
           id: string
           invited_at: string
-          invited_by: string
           is_admin: boolean | null
           project_id: string
           status: string
@@ -247,10 +245,8 @@ export type Database = {
           access_level?: string | null
           activated_at?: string | null
           created_at?: string | null
-          created_by?: string | null
           id?: string
           invited_at?: string
-          invited_by: string
           is_admin?: boolean | null
           project_id: string
           status: string
@@ -260,30 +256,14 @@ export type Database = {
           access_level?: string | null
           activated_at?: string | null
           created_at?: string | null
-          created_by?: string | null
           id?: string
           invited_at?: string
-          invited_by?: string
           is_admin?: boolean | null
           project_id?: string
           status?: string
           user_id?: string
         }
         Relationships: [
-          {
-            foreignKeyName: "dev_project_users_created_by_fkey"
-            columns: ["created_by"]
-            isOneToOne: false
-            referencedRelation: "dev_profiles"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "dev_project_users_invited_by_fkey"
-            columns: ["invited_by"]
-            isOneToOne: false
-            referencedRelation: "dev_profiles"
-            referencedColumns: ["id"]
-          },
           {
             foreignKeyName: "dev_project_users_project_id_fkey"
             columns: ["project_id"]
@@ -805,10 +785,8 @@ export type Database = {
           access_level: string | null
           activated_at: string | null
           created_at: string | null
-          created_by: string | null
           id: string
           invited_at: string
-          invited_by: string
           is_admin: boolean | null
           project_id: string
           status: string
@@ -818,10 +796,8 @@ export type Database = {
           access_level?: string | null
           activated_at?: string | null
           created_at?: string | null
-          created_by?: string | null
           id?: string
           invited_at?: string
-          invited_by: string
           is_admin?: boolean | null
           project_id: string
           status: string
@@ -831,30 +807,14 @@ export type Database = {
           access_level?: string | null
           activated_at?: string | null
           created_at?: string | null
-          created_by?: string | null
           id?: string
           invited_at?: string
-          invited_by?: string
           is_admin?: boolean | null
           project_id?: string
           status?: string
           user_id?: string
         }
         Relationships: [
-          {
-            foreignKeyName: "project_users_created_by_fkey"
-            columns: ["created_by"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "project_users_invited_by_fkey"
-            columns: ["invited_by"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
-          },
           {
             foreignKeyName: "project_users_project_id_fkey"
             columns: ["project_id"]
@@ -1157,9 +1117,15 @@ export type Database = {
     }
     Functions: {
       dev_is_global_admin: {
-        Args: {
-          user_uuid: string
-        }
+        Args: { user_uuid: string }
+        Returns: boolean
+      }
+      dev_is_global_project_admin: {
+        Args: { user_uuid: string }
+        Returns: boolean
+      }
+      dev_is_project_admin: {
+        Args: { user_uuid: string; project_uuid: string }
         Returns: boolean
       }
       get_current_user_role: {
@@ -1167,23 +1133,19 @@ export type Database = {
         Returns: string
       }
       is_global_admin: {
-        Args: {
-          user_uuid: string
-        }
+        Args: { user_uuid: string }
+        Returns: boolean
+      }
+      is_global_project_admin: {
+        Args: { user_uuid: string }
         Returns: boolean
       }
       is_project_admin: {
-        Args: {
-          user_uuid: string
-          project_uuid: string
-        }
+        Args: { user_uuid: string; project_uuid: string }
         Returns: boolean
       }
       user_has_form_access: {
-        Args: {
-          user_uuid: string
-          form_uuid: string
-        }
+        Args: { user_uuid: string; form_uuid: string }
         Returns: boolean
       }
     }
@@ -1207,27 +1169,29 @@ export type Database = {
   }
 }
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -1235,20 +1199,22 @@ export type Tables<
     : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -1256,20 +1222,22 @@ export type TablesInsert<
     : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -1277,21 +1245,23 @@ export type TablesUpdate<
     : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof PublicSchema["CompositeTypes"]
+    | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
@@ -1300,6 +1270,26 @@ export type CompositeTypes<
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
+
+export const Constants = {
+  public: {
+    Enums: {
+      form_status: ["draft", "active", "closed"],
+      notification_status: ["sent", "failed", "retrying"],
+      notification_type: ["email", "whatsapp"],
+      service_status: ["healthy", "degraded", "down"],
+      task_status: ["pending", "in_progress", "completed"],
+      user_role: ["global_admin", "project_admin", "user", "approver"],
+      user_role_old: [
+        "admin",
+        "user",
+        "approver",
+        "global_admin",
+        "project_admin",
+      ],
+    },
+  },
+} as const
